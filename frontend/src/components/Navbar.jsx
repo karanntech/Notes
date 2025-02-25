71ea3aa3c94ec45490f5f7811787f27e5af8fbc1
karann09 <karankumar011kp@gmail.com>
2025-02-25 14:48:45 +0530

    created function on Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ProfileInfo from './ProfileInfo.jsx'
+import { useNavigate } from 'react-router-dom'
+import Searchbar from './Searchbar.jsx';
 
 const Navbar = () => {
+
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const navigate = useNavigate;
+
+  const onLogout = () => {
+    navigate("/login");
+  };
+
+  const handleSearch = () => {
+
+  };
+
+  const onClearSearch = () => {
+    setSearchQuery("")
+  }
+
   return (
     <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow'>
         <h2 className='text-xl font-medium text-black py-2'>Notes</h2>
+
+        <Searchbar value={searchQuery} onChange={({target})=> {
+          setSearchQuery(target.value);
+        }}
+        handleSearch={handleSearch}
+        onClearSearch={onClearSearch}
+        />
+
+        <ProfileInfo onLogout={onLogout}/>
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
---
+++ frontend/src/components/ProfileInfo.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { getInitials } from '../utils/helper.js';
+
+const ProfileInfo = (onLogout) => {
+  return (
+    <div className='flex items-center gap-3'>
+        <div className='w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-200'>
+            {getInitials("John William")}
+        </div>
+        <div>
+            <p className='text-sm font-medium'>Name</p>
+            <button className='text-sm text-slate-700 cursor-pointer underline' onClick={onLogout}>Logout</button>
+        </div>
+    </div>
+  )
+}
+
+export default ProfileInfo;
