6f3736cd070055b66a2f0b1737a8bd5500804bf1
karann09 <karankumar011kp@gmail.com>
2025-02-28 20:57:55 +0530

    Edit note API

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -123,8 +123,47 @@ const addNote = async(req, res) => {
     }
 }
 
+const editNote = async(req, res) => {
+    const noteId = req.params.noteId;
+    const {title, content, tags, isPinned} = req.body;
+    const {user} = req.user;
+
+    if(!title && !content && !tags){
+        return res
+        .status(400)
+        .json({error: true, message: "No changes provided"});
+    }
+
+    try {
+        const note = await Note.findOne({_id: noteId, userId: user._id});
+        if(!note){
+            return res.status(404).json({error: true, message: "Note not found"})
+        }
+
+        if(title) note.title = title;
+        if(content) note.content = content;
+        if(tags) note.tags = tags;
+        if(isPinned) note.isPinned = isPinned;
+
+        await note.save();
+
+        return res.json({
+            error: false,
+            note,
+            message: "Note updated"
+        })
+    } catch (error) {
+        return res.status(500).json({
+            error: true,
+            message: "Internal server error"
+        })
+    }
+
+}
+
 export {
     register,
     login,
-    addNote
+    addNote,
+    editNote
 }
