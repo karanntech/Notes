f7cfa38b7d98a2e28976b22cfffb013665b8a22a
karann09 <karankumar011kp@gmail.com>
2025-03-02 22:27:25 +0530

    fix minor bug

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -37,7 +37,7 @@ const Login = () => {
         navigate('/dashboard')
       }
     } catch (error) {
-      if(error.response && error.response.data && error.data.response.message){
+      if(error.response && error.response.data && error.response.data.message){
         setError(error.response.data.message)
       }else{
         setError("An unexpected error occured. Please try again.")
