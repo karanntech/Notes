4e780172900410f422135e2582d847cfca8f08ac
karann09 <karankumar011kp@gmail.com>
2025-03-04 21:18:05 +0530

    add func. in NoteCard

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {MdOutlinePushPin, MdCreate, MdDelete} from 'react-icons/md'
+import {MdOutlinePushPin, MdCreate, MdDelete} from 'react-icons/md';
+import moment from  "moment";
 
 const NoteCard = ({title, date, content, tags, isPinned, onEdit, onDelete, onPinNote}) => {
   return (
@@ -7,13 +8,17 @@ const NoteCard = ({title, date, content, tags, isPinned, onEdit, onDelete, onPin
         <div className='flex items-center justify-between'>
             <div>
                 <h6 className='text-sm font-medium'>{title}</h6>
-                <span className='text-xs text-slate-500'>{date}</span>
+                <span className='text-xs text-slate-500'>
+                    {moment(date).format('DD MMM YYYY')}
+                </span>
             </div>
             <MdOutlinePushPin className={`icon-btn ${isPinned ? 'text-[#2B85FF]' : 'text-slate-300'}`} onClick={onPinNote}/>
         </div>
         <p className='text-xs text-slate-600 mt-2'>{content?.slice(0,60)}</p>
         <div className='flex items-center justify-between mt-2'>
-            <div className='text-xs text-slate-500'>{tags}</div>
+            <div className='text-xs text-slate-500'>
+                {tags.map((item)=> `#${item}`)}
+            </div>
             <div className='flex items-center gap-2'>
                 <MdCreate className='icon-btn hover:text-green-600' onClick={onEdit}/>
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,11 @@
-import React, { useEffect, useState } from 'react'
-import Navbar from '../components/Navbar.jsx';
-import NoteCard from '../components/NoteCard.jsx';
-import { MdAdd } from 'react-icons/md';
-import AddEditNotes from './AddEditNotes.jsx';
-import Modal from "react-modal"
-import { useNavigate } from 'react-router-dom';
-import axiosInstance from '../utils/axiosInstance.js';
-
-
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar.jsx";
+import NoteCard from "../components/NoteCard.jsx";
+import { MdAdd } from "react-icons/md";
+import AddEditNotes from "./AddEditNotes.jsx";
+import Modal from "react-modal";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../utils/axiosInstance.js";
 
 const Home = () => {
 
@@ -23,90 +21,91 @@ const Home = () => {
   const navigate = useNavigate();
 
   //Get user Info
-  const getUserInfo = async() => {
+  const getUserInfo = async () => {
     try {
       const response = await axiosInstance.get("/get-user");
-      if(response.data && response.data.user){
-        setUserInfo(response.data.user)
+      if (response.data && response.data.user) {
+        setUserInfo(response.data.user);
       }
     } catch (error) {
-      if(error.response.status === 401){
+      if (error.response.status === 401) {
         localStorage.clear();
         navigate("/login");
-      };
-    };
+      }
+    }
   };
 
-//Get all notes
+  //Get all notes
 
-const getAllNotes = async() => {
-  try {
-    const response = await axiosInstance.get("/get-all-notes");
-    if(response.data && response.data.notes){
-      setAllNotes(response.data.notes)
+  const getAllNotes = async () => {
+    try {
+      const response = await axiosInstance.get("/get-all-notes");
+      if (response.data && response.data.notes) {
+        setAllNotes(response.data.notes);
+      }
+    } catch (error) {
+      console.log("An unexpected error occured. Please try again");
     }
-  } catch (error) {
-    console.log("An unexpected error occured. Please try again")
-    }
-  }
-
+  };
 
   useEffect(() => {
-  getAllNotes();
-  getUserInfo();
-    return () => {
-    }
-  }, [])
-  
+    getAllNotes();
+    getUserInfo();
+    return () => {};
+  }, []);
 
   return (
     <>
-      <Navbar userInfo={userInfo}/>
+      <Navbar userInfo={userInfo} />
 
-      <div className='container mx-auto'>
-
-        <div className='grid grid-cols-3 gap-4 m-8'>
-        <NoteCard 
-          title="Meeting on 7th March"
-          date="26 Feb 2025"
-          content="Meeting for Interview via Meet"
-          tags="#meeting"
-          isPinned={true}
-          onEdit={()=>{}}
-          onDelete={()=>{}}
-          onPinNote={()=>{}}
-        />
+      <div className="container mx-auto">
+        <div className="grid grid-cols-3 gap-4 m-8">
+          {allNotes.map((item, index) => (
+            <NoteCard
+            key={item._id}
+              title={item.title}
+              date={item.createdOn}
+              content={item.content}
+              tags={item.tags}
+              isPinned={item.isPinned}
+              onEdit={() => {}}
+              onDelete={() => {}}
+              onPinNote={() => {}}
+            />
+          ))}
         </div>
       </div>
 
-      
-      <button className='w-16 h-16 flex items-center justify-center rounded-xl bg-[#2B85FF] hover:bg-blue-600 absolute right-10 bottom-10 cursor-pointer' onClick={()=> {
-        setOpenAddEditModal({isShown: true, type: "add", data: null})
-      }}>
-        <MdAdd className='text-[32px] text-white'/>
+      <button
+        className="w-16 h-16 flex items-center justify-center rounded-xl bg-[#2B85FF] hover:bg-blue-600 absolute right-10 bottom-10 cursor-pointer"
+        onClick={() => {
+          setOpenAddEditModal({ isShown: true, type: "add", data: null });
+        }}
+      >
+        <MdAdd className="text-[32px] text-white" />
       </button>
 
-      <Modal 
-        isOpen = {openAddEditModal.isShown}
-        onRequestClose = {()=> {}}
+      <Modal
+        isOpen={openAddEditModal.isShown}
+        onRequestClose={() => {}}
         style={{
-          overlay :{
+          overlay: {
             backgroundColor: "rgba(0,0,0,0.2)",
           },
         }}
-          contentLabel=""
-          className="w-[40%] max-h-3/4 bg-white rounded-2xl mx-auto mt-14 p-5"
+        contentLabel=""
+        className="w-[40%] max-h-3/4 bg-white rounded-2xl mx-auto mt-14 p-5"
       >
-      <AddEditNotes
-        type={openAddEditModal.type}
-        noteData={openAddEditModal.data}
-        onClose={() => {
-          setOpenAddEditModal({isShown: false, type: "add", data: null})
-        }}
-      />
-    </Modal>
+        <AddEditNotes
+          type={openAddEditModal.type}
+          noteData={openAddEditModal.data}
+          onClose={() => {
+            setOpenAddEditModal({ isShown: false, type: "add", data: null });
+          }}
+        />
+      </Modal>
     </>
-  )
-}
+  );
+};
 
 export default Home;
